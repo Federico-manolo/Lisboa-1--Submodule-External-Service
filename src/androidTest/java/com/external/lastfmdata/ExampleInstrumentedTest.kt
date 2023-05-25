6f510ea7689cf8ea90c275edfe10f958edbf61ff
diegojuarez965 <diegohernanjuarez965@gmail.com>
2023-05-24 22:13:15 -0300

    test commit

--- src/androidTest/java/com/external/lastfmdata/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com/external/lastfmdata/ExampleInstrumentedTest.kt
@@ -17,7 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the app under test2.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.external.lastfmdata", appContext.packageName)
     }
